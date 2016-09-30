a615a5ce0ff09de0d2f72fbba6489c4c11246f18
nhhuy95 <nhhuy1995@gmail.com>
2016-09-30 17:29:06 +0700

    huy commit

--- client/app/controllers/NotfoundController.php
+++ client/app/controllers/NotfoundController.php
@@ -1,14 +1,16 @@
 <?php
 namespace DjClient\Controller;
+
 use DjClient\Library\Helper;
 use Phalcon\Mvc\View;
+
 class NotfoundController extends ControllerBase
 {
     public function show404Action()
     {
         $this->response->setStatusCode(404, 'Not Found');
-    	// $this->view->setRenderLevel(View::LEVEL_ACTION_VIEW);
-       	// $this->view->disableLevel(View::LEVEL_MAIN_LAYOUT);
+//        $this->view->setRenderLevel(View::LEVEL_ACTION_VIEW);
+        // $this->view->disableLevel(View::LEVEL_MAIN_LAYOUT);
         $this->view->header = Helper::setHeader("Không tìm thấy trang");
     }
 }
